8ab31a4ebd71fd8ad5eb73359d2d2a475c6c14e1
Bharat Kumar <kumarbharat661@gmail.com>
2022-06-24 19:35:30 +0530

    handlers added

--- src/containers/Newsletter.jsx
+++ src/containers/Newsletter.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import newsletter from "../assets/newsletter.png";
 
 const Newsletter = () => {
+  const [email, setEmail] = useState("");
+  //   Submit event
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/email/newsletter", {
+        email: email,
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="w-full h-full flex flex-col md:flex-row px-[60px] py-[60px] bg-background-4 bg-cover bg-no-repeat bg-center">
       <div className="flex flex-1 justify-center items-start flex-col mr-[50px]">
@@ -12,7 +26,10 @@ const Newsletter = () => {
           Get our minimal and beautiful newsletter straight to your inbox every
           Tuesday.
         </p>
-        <form className="w-full animate__animated animate__fadeInUp">
+        <form
+          className="w-full animate__animated animate__fadeInUp"
+          onSubmit={handleSubmit}
+        >
           <div className="flex items-start flex-col justify-between gap-2">
             {/* <input
                   type="text"
@@ -23,9 +40,13 @@ const Newsletter = () => {
               type="email"
               className="w-[90%]  md:w-[70%] py-[12px] px-[16px] border border-gray-300 rounded-md placeholder:font-Inter placeholder:font-light focus:outline-none focus:shadow-outline shadow appearance-none font-Inter"
               placeholder="Enter your email address"
+              onChange={(e) => setEmail(e.target.value)}
             />
             <div className="md:mt-[32px] mt-[20px]">
-              <button className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+              <button
+                type="submit"
+                className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+              >
                 <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
                 <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                 <span className="relative text-white font-Space">
